a433db53069d68839f9abc26500daf1edd842ded
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-04-29 15:55:58 +0800

    半透明和多边形按钮

--- ClientGUI/XNAClientButton.cs
+++ ClientGUI/XNAClientButton.cs
@@ -4,13 +4,24 @@ using Rampastring.Tools;
 using System;
 using ClientCore;
 using ClientCore.Extensions;
+using Microsoft.Xna.Framework;
+using System.Linq;
 
 namespace ClientGUI
 {
+    public enum ActiveMode
+    {
+        Normal,
+        NotTransparencyPixel,
+        Polygon,
+    }
+
     public class XNAClientButton : XNAButton, IToolTipContainer
     {
         public ToolTip ToolTip { get; private set; }
 
+        private bool _lastEnterStatus = false;
+        private readonly static WeakReference<Color[]> _colorsCache = new([]);
         private string _initialToolTipText;
         public string ToolTipText
         {
@@ -23,6 +34,9 @@ namespace ClientGUI
                     _initialToolTipText = value;
             }
         }
+        public ActiveMode ActiveMode { get; set; } = ActiveMode.Normal;
+
+        private Point[] _polygonPoints = [];
 
         public XNAClientButton(WindowManager windowManager) : base(windowManager)
         {
@@ -53,19 +67,174 @@ namespace ClientGUI
 
         protected override void ParseControlINIAttribute(IniFile iniFile, string key, string value)
         {
-            if (key == "MatchTextureSize" && Conversions.BooleanFromString(value, false))
+            switch (key)
             {
-                Width = IdleTexture.Width;
-                Height = IdleTexture.Height;
-                return;
-            }
-            else if (key == "ToolTip")
-            {
-                ToolTipText = value.FromIniString();
-                return;
+                case "MatchTextureSize" when Conversions.BooleanFromString(value, false):
+                    Width = IdleTexture.Width;
+                    Height = IdleTexture.Height;
+                    return;
+                case "ToolTip":
+                    ToolTipText = value.FromIniString();
+                    return;
+                case nameof(ActiveMode) when Enum.TryParse<ActiveMode>(value, out var activeMode):
+                    ActiveMode = activeMode;
+                    return;
+                case "PolygonPoints":
+                    _polygonPoints = value
+                        .Split(':')
+                        .Select(s => s.Split(','))
+                        .Select(a => new Point(
+                            int.Parse(a[0].Trim()),
+                            int.Parse(a[1].Trim())))
+                        .ToArray();
+                    return;
             }
 
             base.ParseControlINIAttribute(iniFile, key, value);
+        }
+
+        public override void OnMouseEnter()
+        {
+            switch (ActiveMode)
+            {
+                case ActiveMode.NotTransparencyPixel:
+                case ActiveMode.Polygon:
+                    var current = CheckActive();
+                    if ((current, _lastEnterStatus) is (true, false))
+                        base.OnMouseEnter();
+                    _lastEnterStatus = current;
+                    break;
+                case ActiveMode.Normal:
+                default:
+                    base.OnMouseEnter();
+                    break;
+            }
+        }
+
+        public override void OnMouseLeave()
+        {
+            switch (ActiveMode)
+            {
+                case ActiveMode.NotTransparencyPixel:
+                case ActiveMode.Polygon:
+                    var current = CheckActive();
+                    if ((current, _lastEnterStatus) is (false, true))
+                        base.OnMouseLeave();
+                    _lastEnterStatus = current;
+                    break;
+                case ActiveMode.Normal:
+                default:
+                    base.OnMouseLeave();
+                    break;
+            }
+        }
+
+        public override void OnMouseMove()
+        {
+            switch (ActiveMode)
+            {
+                case ActiveMode.NotTransparencyPixel:
+                case ActiveMode.Polygon:
+                    var current = CheckActive();
+                    switch ((current, _lastEnterStatus))
+                    {
+                        case (true, true):
+                            base.OnMouseMove();
+                            _lastEnterStatus = current;
+                            break;
+                        case (false, true):
+                            OnMouseLeave();
+                            break;
+                        case (true, false):
+                            OnMouseEnter();
+                            break;
+                        case (false, false):
+                        default:
+                            _lastEnterStatus = current;
+                            break;
+                    }
+
+                    break;
+                case ActiveMode.Normal:
+                default:
+                    base.OnMouseMove();
+                    break;
+            }
+        }
+
+        public override void OnLeftClick()
+        {
+            switch (ActiveMode)
+            {
+                case ActiveMode.NotTransparencyPixel:
+                case ActiveMode.Polygon:
+                    var current = CheckActive();
+                    if (current)
+                        base.OnLeftClick();
+                    break;
+                case ActiveMode.Normal:
+                default:
+                    base.OnLeftClick();
+                    break;
+            }
+        }
+
+        private bool CheckActive()
+        {
+            switch (ActiveMode)
+            {
+                case ActiveMode.NotTransparencyPixel:
+                    {
+                        Point checkPoint = GetCursorPoint();
+                        if (!IdleTexture.Bounds.Contains(checkPoint))
+                            return false;
+
+                        var size = IdleTexture.Width * IdleTexture.Height;
+                        _colorsCache.TryGetTarget(out var color);
+                        if (color is null || color.Length < size)
+                            color = new Color[size];
+                        _colorsCache.SetTarget(color);
+
+                        var index = IdleTexture.Width * checkPoint.Y + checkPoint.X;
+                        IdleTexture.GetData(color);
+
+                        return color[index].A != 0;
+                    }
+                case ActiveMode.Polygon:
+                    {
+                        Point checkPoint = GetCursorPoint();
+                        bool inside = false;
+
+                        int pointCount = _polygonPoints.Length;
+                        for (int i = 0, j = pointCount - 1; i < pointCount; j = i, i++)
+                        {
+                            ref Point p1 = ref _polygonPoints[i];
+                            ref Point p2 = ref _polygonPoints[j];
+                            if (checkPoint.Y < p2.Y)
+                            {
+                                if (p1.Y <= checkPoint.Y)
+                                {
+                                    if ((checkPoint.Y - p1.Y) * (p2.X - p1.X) > (checkPoint.X - p1.X) * (p2.Y - p1.Y))
+                                    {
+                                        inside = !inside;
+                                    }
+                                }
+                            }
+                            else if (checkPoint.Y < p1.Y)
+                            {
+                                if ((checkPoint.Y - p1.Y) * (p2.X - p1.X) < (checkPoint.X - p1.X) * (p2.Y - p1.Y))
+                                {
+                                    inside = !inside;
+                                }
+                            }
+                        }
+
+                        return inside;
+                    }
+                default:
+                    return true;
+            }
+
         }
     }
 }
