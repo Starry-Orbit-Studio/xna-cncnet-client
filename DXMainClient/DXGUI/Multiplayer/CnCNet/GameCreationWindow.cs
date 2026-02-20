58f4798c20bee6f511f06875d530e6444ca3ff61
Uranusian <mikesariitten@gmail.com>
2026-02-20 16:23:22 +0800

    修复隧道服务器不显示的问题

--- DXMainClient/DXGUI/Multiplayer/CnCNet/GameCreationWindow.cs
+++ DXMainClient/DXGUI/Multiplayer/CnCNet/GameCreationWindow.cs
@@ -289,6 +289,8 @@ namespace DTAClient.DXGUI.Multiplayer.CnCNet
             lbTunnelList.Enable();
             btnDisplayAdvancedOptions.Disable();
 
+            tunnelHandler.RefreshTunnelsAsync();
+
             SetAttributesFromIni();
 
             CenterOnParent();
